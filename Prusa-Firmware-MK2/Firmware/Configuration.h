3f1bf9a50b3df835139285d662e8cff3efcea58b
Ian Koh <ian_koh93@hotmail.com>
2017-11-20 10:08:37 -0800

    Changed the Author and the name of the machine

--- Prusa-Firmware-MK2/Firmware/Configuration.h
+++ Prusa-Firmware-MK2/Firmware/Configuration.h
@@ -5,7 +5,7 @@
 #include "Configuration_prusa.h"
 
 // Firmware version
-#define FW_version "3.0.10-alpha-2"
+#define FW_version "3.0.10 - IAN"
 
 #define FW_PRUSA3D_MAGIC "PRUSA3DFW"
 #define FW_PRUSA3D_MAGIC_LEN 10
@@ -67,7 +67,7 @@
 //#define STRING_VERSION "1.0.2"
 
 #define STRING_VERSION_CONFIG_H __DATE__ " " __TIME__ // build date and time
-#define STRING_CONFIG_H_AUTHOR "(none, default config)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(IAN KOH, UBC RAPID)" // Who made the changes.
 
 // SERIAL_PORT selects which serial port should be used for communication with the host.
 // This allows the connection of wireless adapters (for instance) to non-default port pins.
